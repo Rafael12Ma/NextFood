1e184c710afcb34c461ef237ffe9c2bd5d4f30ac
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2024-06-21 11:16:49 +0300

    21.6 changes

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,24 @@
+import MealsGrid from "@/Components/main-header/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
 export default function ShareMealsPage() {
   return (
     <>
-      <main>Share Meals Page</main>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe!</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
